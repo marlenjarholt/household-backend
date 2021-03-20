dfd558047c2f6be1068076210e2c8114ef3e64be
marlenjarholt <pusikatt@live.no>
2021-03-20 17:13:59 +0100

    working databse

--- src/main/kotlin/no/householdBackend/HouseholdApplication.kt
+++ src/main/kotlin/no/householdBackend/HouseholdApplication.kt
@@ -3,9 +3,11 @@ package no.householdBackend
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.dropwizard.Application
 import io.dropwizard.Configuration
+import io.dropwizard.db.DataSourceFactory
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import no.householdBackend.household.GetHousehold
+import org.flywaydb.core.Flyway
 
 fun main(args: Array<String>){
     HouseholdApplication().run(*args)
@@ -13,7 +15,11 @@ fun main(args: Array<String>){
 
 class HouseholdApplication : Application<HouseholdConfig>() {
     override fun run(configuration: HouseholdConfig, environment: Environment) {
-        print(configuration.s)
+        val dataSource = configuration.database.build(environment.metrics(), "household")
+        Flyway.configure()
+            .dataSource(dataSource)
+            .load()
+            .migrate()
 
         environment.jersey().register(GetHousehold())
     }
@@ -25,5 +31,5 @@ class HouseholdApplication : Application<HouseholdConfig>() {
 }
 
 data class HouseholdConfig(
-    val s:String
+    val database: DataSourceFactory
 ) :Configuration()
