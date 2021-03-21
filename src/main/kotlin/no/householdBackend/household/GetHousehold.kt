f8df49e9260bcab87c03f288ce86adc0379d88f5
Simen Fonnes <mrsimenfonnes@gmail.com>
2021-03-21 13:41:15 +0100

    add get household

--- src/main/kotlin/no/householdBackend/household/GetHousehold.kt
+++ src/main/kotlin/no/householdBackend/household/GetHousehold.kt
@@ -2,11 +2,13 @@ package no.householdBackend.household
 
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.andThen
 import com.github.michaelbull.result.mapBoth
 import com.github.michaelbull.result.mapError
 import com.github.michaelbull.result.runCatching
 import org.jdbi.v3.core.Jdbi
+import org.slf4j.LoggerFactory
 import java.lang.Exception
 import java.util.*
 import java.util.stream.Collectors
@@ -17,7 +19,6 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.Response.Status.NOT_FOUND
-import org.slf4j.LoggerFactory
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/household/{id}")
@@ -27,49 +28,106 @@ class GetHousehold(private val jdbi: Jdbi) {
     @GET
     fun getHousehold(@PathParam("id") id: UUID): Response =
         runCatching {
-            jdbi.withHandle<Household?, Exception> {
+            jdbi.withHandle<List<DatabaseRow>, Exception> {
                 it.createQuery(
                     """
-                    |SELECT *
+                    |SELECT
+                    |   households.id AS householdId,
+                    |   households.name AS householdName,
+                    |   households.refrigerator_id AS householdRefrigeratorId,
+                    |   refrigerators.id AS refrigeratorId,
+                    |   refrigerator_grocery_relation.refrigerator_id AS refrigeratorGroceryRelationId,
+                    |   groceries.id AS groceryId,
+                    |   groceries.name AS groceryName,
+                    |   groceries.amount AS groceryAmount,
+                    |   groceries.unit AS groceryUnit
                     |FROM households
-                    |WHERE id = :id;
-                """.trimMargin()
+                    |JOIN refrigerators ON refrigerators.id = households.refrigerator_id
+                    |JOIN refrigerator_grocery_relation ON refrigerator_grocery_relation.refrigerator_id = refrigerators.id
+                    |JOIN groceries ON groceries.id = refrigerator_grocery_relation.grocery_id
+                    |WHERE households.id = :id;
+                    """.trimMargin()
                 )
                     .bind("id", id)
                     .map { rs, _, _ ->
-                        Household(
-                            id = UUID.fromString(rs.getString("id")),
-                            name = rs.getString("name")
+                        DatabaseRow(
+                            householdId = UUID.fromString(rs.getString("householdId")),
+                            householdName = rs.getString("householdName"),
+                            householdRefrigeratorId = UUID.fromString(rs.getString("householdRefrigeratorId")),
+                            refrigeratorId = UUID.fromString(rs.getString("refrigeratorId")),
+                            refrigeratorGroceryRelationId = UUID.fromString(rs.getString("refrigeratorGroceryRelationId")),
+                            groceryId = UUID.fromString(rs.getString("groceryId")),
+                            groceryName = rs.getString("groceryName"),
+                            groceryAmount = rs.getDouble("groceryAmount"),
+                            groceryUnit = rs.getString("groceryUnit")
                         )
                     }
                     .collect(Collectors.toList())
-                    .firstOrNull()
             }
         }.mapError {
             DBError(it)
         }.andThen {
-            if (it != null) {
-                Ok(it)
-            } else {
+            if (it.isEmpty()) {
                 Err(NotFound)
+            } else {
+                Ok(it)
             }
-        }.mapBoth(
-            success = {
-                Response.ok(it).build()
-            },
-            failure = {
-                when (it) {
-                    is DBError -> {
-                        logger.error("Database error:", it.error)
-                        Response.serverError().build()
+        }.andThen(::createHousehold)
+            .mapBoth(
+                success = {
+                    Response.ok(it).build()
+                },
+                failure = {
+                    when (it) {
+                        is DBError -> {
+                            logger.error("Database error:", it.error)
+                            Response.serverError().build()
+                        }
+                        NotFound -> Response.status(NOT_FOUND).build()
+                        is ParseError -> {
+                            logger.error("Parse error:", it.error)
+                            Response.serverError().build()
+                        }
                     }
-                    NotFound -> Response.status(NOT_FOUND).build()
                 }
-            }
-        )
+            )
 }
+
+private fun createHousehold(databaseRows: List<DatabaseRow>): Result<Household, GetHouseholdError> =
+    runCatching {
+        Household(
+            id = databaseRows.first().householdId,
+            name = databaseRows.first().householdName,
+            refrigerator = Refrigerator(
+                id = databaseRows.first().refrigeratorId,
+                groceries = databaseRows.map {
+                    Grocery(
+                        id = it.groceryId,
+                        name = it.groceryName,
+                        amount = it.groceryAmount,
+                        unit = it.groceryUnit
+                    )
+                }
+            )
+        )
+    }.mapError {
+        ParseError(it)
+    }
+
+data class DatabaseRow(
+    val householdId: UUID,
+    val householdName: String,
+    val householdRefrigeratorId: UUID,
+    val refrigeratorId: UUID,
+    val refrigeratorGroceryRelationId: UUID,
+    val groceryId: UUID,
+    val groceryName: String,
+    val groceryAmount: Double,
+    val groceryUnit: String
+)
 
 private sealed class GetHouseholdError
 
 private class DBError(val error: Throwable) : GetHouseholdError()
 private object NotFound : GetHouseholdError()
+private class ParseError(val error: Throwable) : GetHouseholdError()
--- src/main/kotlin/no/householdBackend/household/Household.kt
+++ src/main/kotlin/no/householdBackend/household/Household.kt
@@ -1,8 +1,21 @@
 package no.householdBackend.household
 
-import java.util.*
+import java.util.UUID
 
 data class Household(
     val id: UUID,
-    val name: String
+    val name: String,
+    val refrigerator: Refrigerator
+)
+
+data class Refrigerator(
+    val id: UUID,
+    val groceries: List<Grocery>
+)
+
+data class Grocery(
+    val id: UUID,
+    val name: String,
+    val amount: Number,
+    val unit: String
 )
